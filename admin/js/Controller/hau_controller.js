185afcd6a277bf6db2cb8ab59eb9a844c35f5769
hau <lnhau94@gmail.com>
2022-11-01 19:04:07 +0700

    hau

--- admin/js/Controller/hau_controller.js
+++ admin/js/Controller/hau_controller.js
@@ -4,14 +4,14 @@ import { adminProductView } from "../View/hau_product.js";
 import { adminAccountView } from "../View/hau_account.js";
 
 export const controller = {
-    model,view,
-    showProductPage(){
-        document.getElementById("hau-content-page").innerHTML =`
+    model, view,
+    showProductPage() {
+        document.getElementById("hau-content-page").innerHTML = `
             ${adminProductView.renderProductPageHeader("Product")}
             ${adminProductView.renderProductTable()}
         `;
         adminProductView.showProduct(model.keyboards);
-        document.querySelector("#addbtn").addEventListener("click", () =>{
+        document.querySelector("#addbtn").addEventListener("click", () => {
             document.querySelector(".huy-container-info-pd").classList.remove("huy-container-info-pd-none");
             console.log(document.querySelector(".huy-all-frame").classList);
             document.querySelector(".huy-all-frame").classList.remove("huy-close-all-frame");
@@ -19,34 +19,34 @@ export const controller = {
             document.querySelector(".huy-container-info-pd").classList.add("huy-container-info-pd-on");
         });
     },
-    showAccountPage(){
-        document.getElementById("hau-content-page").innerHTML =`
+    showAccountPage() {
+        document.getElementById("hau-content-page").innerHTML = `
             ${adminAccountView.renderAccountPageHeader()}
             ${adminAccountView.renderAccountTable()}
             
         `;
         adminAccountView.showAccount(model.accounts);
     },
-    init(){
+    init() {
         view.init();
         document.getElementById("productBtn").addEventListener("click", () => {
             controller.showProductPage();
             view.changeCurrentBtn(document.getElementById("productBtn"));
-         });
+        });
 
-         document.getElementById("orderBtn").addEventListener("click", () => {
-            
+        document.getElementById("orderBtn").addEventListener("click", () => {
+
             view.changeCurrentBtn(document.getElementById("orderBtn"));
-         });
+        });
 
-         document.getElementById("dashboardBtn").addEventListener("click", () => {
-            
+        document.getElementById("dashboardBtn").addEventListener("click", () => {
+
             view.changeCurrentBtn(document.getElementById("dashboardBtn"));
-         });
+        });
 
-         document.getElementById("accountBtn").addEventListener("click", () => {
+        document.getElementById("accountBtn").addEventListener("click", () => {
             controller.showAccountPage();
             view.changeCurrentBtn(document.getElementById("accountBtn"));
-         });
+        });
     }
 }
