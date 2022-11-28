decbaaa3fb9dc37c3f02f3a6393cf1fc3e29cf85
hau <lnhau94@gmail.com>
2022-11-28 21:06:25 +0700

    edit statitics

--- admin/js/Controller/hau_controller.js
+++ admin/js/Controller/hau_controller.js
@@ -105,6 +105,7 @@ export const controller = {
 
         document.getElementById("dashboardBtn").addEventListener("click", () => {
 
+            statitics.init();
             view.changeCurrentBtn(document.getElementById("dashboardBtn"));
         });
 
